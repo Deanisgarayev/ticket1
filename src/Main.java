a870cb3235e3ee0eae090c27366a9ff0e0575f4d
Денис <garaevdenis2019@gmail.com>
2023-03-27 21:35:55 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        ticket1();
+        ticket2();
+        ticket3();
+        ticket4();
+        ticket5();
+        ticket6();
+    }
+
+    public static void ticket1 () {
+////        Напишите программу, которая переворачивает массив.
+//        Input : 1, 2, 3, 4, 5
+//        Output :5, 4, 3, 2, 1
+//
+//        Input :  10, 20, 30, 40
+//        Output : 40, 30, 20, 10
+    }
+public static void ticket2 () {
+////    Напишите программу, которая проверяет, является ли строка палиндромом.
+//    public static boolean istPalindrom(char[] word){
+//        int i1 = 0;
+//        int i2 = word.length - 1;
+//        while (i2 > i1) {
+//            if (word[i1] != word[i2]) {
+//                return false;
+//            }
+//            ++i1;
+//            --i2;
+//        }
+//        return true;
+//    }
+    }
+
+public static void ticket3 () {
+//    Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов.
+//    split
+    }
+public static void ticket4 () {
+//    Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.
+ }
+
+public static void ticket5 () {
+//    Необходимо реализовать следующий метод:
+//    1.	Получаем на входе массив чисел.
+//    2.	Все четные числа увеличиваем на единицу.
+//    3.	Возвращаем кусок списка с 3-го по 7-й элемент.
+    }
+public static void ticket6 () {
+//    Напишите метод, который возводит число в квадрат.
+    }
+}
