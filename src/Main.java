c51e5bc7b235843d7c7c5121c9fbe14e0176d225
Денис <garaevdenis2019@gmail.com>
2023-04-02 00:23:59 +0500

    Проверьте задачу по билету 5

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,36 @@
 import java.util.Arrays;
 
 public class Main {
-    public static int[] printArr() {
-        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int i = 0; i < numbers.length; i++) {
-        }
-        return numbers;
-    }
-
+    public static int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     public static void main(String[] args) {
-        int[] arr = printArr();
-        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            int number = arr[i];
-            plusNumber(number);
+        printNumbers(numbers);
+        plusNumbers(numbers);
+        printMiddleNumbers(numbers);
+    }
+
+    public static void printNumbers(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            int number = numbers[i];
         }
-        String space = "";
-        System.out.println(space);
-        for (int i = 2; i < arr[6]; i++) {
-            int number = arr[i];
-            printMiddleArr(number);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void plusNumbers(int[] numbers) {
+        for (int i = 0; i < numbers.length; i = i + 2) {
+            int number = numbers[i];
+            number = number + 2;
+            System.out.println(number);
         }
     }
 
-    public static void plusNumber(int numberA) {
-        if (numberA % 2 == 0) {
-            numberA ++;
-            System.out.println(numberA);
+    public static void printMiddleNumbers(int[] numbers) {
+        for (int i = 2; i < numbers[6]; i++) {
+            int number = numbers[i];
+            System.out.println(number);
         }
     }
-    public static void printMiddleArr(int numberB) {
-        System.out.println(numberB);
-        }
-    }
+}
 
 
 
